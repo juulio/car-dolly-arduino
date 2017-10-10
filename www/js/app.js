c2ef8dc9966b52322d754d069d633dc5f955c428
Julio Del Valle <juulio@gmail.com>
2017-10-09 23:13:40 -0600

    Adding CSS for two blocks

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,15 @@ involtListenForPin[0] = function(index, value){
 //do something when received value from pin A0...
 // console.log(index);
 	// console.log(value);
-	$('.nissan').css({'left':value})
 
+	// Simulate Horizontal Scrolling
+	// $('.main-background').css({'left': -value})
+
+	if(value > 100 && value < 300) {
+		$('.block1').css({'left':value});
+	}
+
+	if(value > 200) {
+		$('.block2').css({'left':value*1.1});
+	}
 };
