2b5b69da4a0400eadfdb962d3249d8b5ad344ad2
Julio Del Valle <juulio@gmail.com>
2017-10-10 00:00:23 -0600

    Adding horizontal parallax example code

--- www/js/app.js
+++ www/js/app.js
@@ -1,10 +1,12 @@
 involtListenForPin[0] = function(index, value){
 //do something when received value from pin A0...
-// console.log(index);
-	// console.log(value);
+	console.log('Sensor Position: ' + value);
 
 	// Simulate Horizontal Scrolling
 	// $('.main-background').css({'left': -value})
+
+	// $('#wrapper').scrollTo(	value*4, 1, {axis:'Y'} );
+	$.scrollTo(	value*4,  10, {axis:'Y'} );
 
 	if(value > 100 && value < 300) {
 		$('.block1').css({'left':value});
