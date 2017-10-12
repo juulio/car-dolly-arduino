c7c28b4c13b1c31dbec0382031b02397cebd0061
Julio Del Valle <juulio@gmail.com>
2017-10-12 09:51:42 -0600

    Adding styling to video player

--- www/js/app.js
+++ www/js/app.js
@@ -1,18 +1,50 @@
-involtListenForPin[0] = function(index, value){
-//do something when received value from pin A0...
-	console.log('Sensor Position: ' + value);
+// Pulse - Nissan
+// Total "width" = 5300px
 
-	// Simulate Horizontal Scrolling
-	// $('.main-background').css({'left': -value})
 
-	// $('#wrapper').scrollTo(	value*4, 1, {axis:'Y'} );
-	$.scrollTo(	value*4,  10, {axis:'Y'} );
+jQuery( document ).ready(function($) {
 
-	if(value > 100 && value < 300) {
-		$('.block1').css({'left':value});
-	}
+	// Init variables
+	// UI elements
+	let videoElement = document.getElementById('video01');
 
-	if(value > 200) {
-		$('.block2').css({'left':value*1.1});
-	}
-};
+	// let $videoIsPlayingElement = $("#videoIsPlaying");
+	let videoIsPlaying = false;
+	let totalWidth = $('body').height(); // pixels
+	let sensorRange = 1023; // Arduino Leonardo 
+	let scrollingFactor = totalWidth / sensorRange; //  5.18
+
+
+	// Hide the Vertical Scroll Bar
+	$('html').css({'overflow':'hidden'});
+
+	involtListenForPin[0] = function(index, value){
+
+		//do something when received value from pin A0...
+		// console.log('Sensor Position: ' + value);
+
+		// $videoIsPlayingElement.html("Video is Playing: " + videoIsPlaying);
+
+		// Simulate Horizontall scrolling with values from Arduino
+		$.scrollTo(	value*scrollingFactor,  30, {axis:'Y'} );
+
+		if(value > 400 && value < 500 && !videoIsPlaying) {
+			$('#video01').fadeIn( "slow", function() {
+				$(this).height(840);
+				// if (videoElement.webkitRequestFullscreen) {
+    //   				videoElement.webkitRequestFullscreen();
+  		// 		}
+				videoElement.currentTime = 0; // Video plays from the beginning
+				videoElement.play();
+				videoIsPlaying = true;
+			});
+		}
+
+		if(value < 400 || value > 600 && videoIsPlaying){
+			$('#video01').fadeOut( "fast", function() {
+				videoElement.pause();
+				videoIsPlaying = false;
+			});
+		}
+	};
+});
